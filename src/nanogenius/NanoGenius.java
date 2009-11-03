0a3ac822d0fe1482c3486b03eaee28355457b658
siodoni@yahoo.com.br <siodoni@yahoo.com.br@a5f7f788-bef2-11de-8784-f7ad4773dbab>
2009-11-03 16:19:59 +0000

    Ajustes

--- src/nanogenius/NanoGenius.java
+++ src/nanogenius/NanoGenius.java
@@ -8,12 +8,13 @@ public class NanoGenius extends Canvas implements CommandListener {
     private Main game;
     private Command cmdSair;
     private Command cmdLoop;
-    private int largura, altura, percBorda, curBlock, curLayout = 0, curSample = 0, emJogo = 0;
+    private int largura, altura, percBorda, curBlock, curLayout = 0, curSample = 0, emJogo = 0, percDesloc;
     private StringBuffer sequencia = new StringBuffer();
     private static Random random = new Random();
 
     public NanoGenius(Main midlet) {
-        largura = getWidth();
+        percDesloc = (getWidth() * 20) / 100;
+        largura = getWidth() - percDesloc;
         altura = getHeight();
         percBorda = (getWidth() * 10) / 100;
         curBlock = 0;
@@ -30,10 +31,10 @@ public class NanoGenius extends Canvas implements CommandListener {
 
     public void paint(Graphics g) {
         g.setColor(Cor.BRANCO);
-        g.fillRect(0, 0, largura, altura);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
         g.setColor(Cor.PRETO);
-        g.fillArc(0, 0, largura, largura, 0, 360);
+        g.fillArc(percDesloc / 2, 0, largura, largura, 0, 360);
 
         if (curBlock == 1) {
             Escala.tocaNota(Escala.cNat);
@@ -52,27 +53,34 @@ public class NanoGenius extends Canvas implements CommandListener {
         }
 
         g.setColor(curBlock == 1 ? Cor.VERDE : Cor.VERDE_ESCURO);
-        g.fillArc(0, 0, largura - percBorda, largura - percBorda, 90, 90);
+        g.fillArc(percDesloc / 2, 0, largura - percBorda, largura - percBorda, 90, 90);
 
         g.setColor(curBlock == 2 ? Cor.VERMELHO : Cor.VERMELHO_ESCURO);
-        g.fillArc(percBorda, 0, largura - percBorda, largura - percBorda, 360, 90);
+        g.fillArc(percBorda + percDesloc / 2, 0, largura - percBorda, largura - percBorda, 360, 90);
 
         g.setColor(curBlock == 3 ? Cor.AMARELO : Cor.AMARELO_ESCURO);
-        g.fillArc(0, percBorda, largura - percBorda, largura - percBorda, 180, 90);
+        g.fillArc(percDesloc / 2, percBorda, largura - percBorda, largura - percBorda, 180, 90);
 
         g.setColor(curBlock == 4 ? Cor.AZUL : Cor.AZUL_ESCURO);
-        g.fillArc(percBorda, percBorda, largura - percBorda, largura - percBorda, 360, -90);
+        g.fillArc(percBorda + percDesloc / 2, percBorda, largura - percBorda, largura - percBorda, 360, -90);
 
         g.setColor(Cor.PRETO);
-        g.fillArc((largura / 4) - (percBorda / 2), (largura / 4) - (percBorda / 2), (largura / 2) + percBorda, (largura / 2) + percBorda, 0, 360);
+        g.fillArc(((largura / 4) - (percBorda / 2)) + percDesloc / 2, (largura / 4) - (percBorda / 2), (largura / 2) + percBorda, (largura / 2) + percBorda, 0, 360);
 
         g.setColor(curBlock == 5 ? Cor.ROXO : Cor.ROXO_ESCURO);
-        g.fillArc(largura / 3, largura / 3, largura / 3, largura / 3, 0, 360);
+        g.fillArc((largura / 3) + percDesloc / 2, largura / 3, largura / 3, largura / 3, 0, 360);
+
+        g.setColor(Cor.LARANJA_ESCURO);
+        if (sequencia.length() == 0) {
+            g.drawString("", 0, largura + percBorda, Graphics.BOTTOM|Graphics.LEFT);
+        }else{
+            g.drawString("Jogada " + sequencia.length(), 0, largura + percBorda, Graphics.BOTTOM|Graphics.LEFT);
+        }
     }
 
     protected void pausa(int tempo) {
         try {
-            Thread.sleep(tempo);
+            Thread.currentThread().sleep(tempo);
         } catch (InterruptedException ex) {
         }
     }
@@ -110,28 +118,28 @@ public class NanoGenius extends Canvas implements CommandListener {
 
             if (tecla == Canvas.FIRE) {
                 novoJogo();
-            }
+            } else {
 
-            if (sequencia.length() > 0) {
-                if ( ((keyCode == KEY_NUM1) && (sequencia.charAt(curSample) == KEY_NUM1))
-                  || ((keyCode == KEY_NUM3) && (sequencia.charAt(curSample) == KEY_NUM2))
-                  || ((keyCode == KEY_NUM5) && (sequencia.charAt(curSample) == KEY_NUM5))
-                  || ((keyCode == KEY_NUM7) && (sequencia.charAt(curSample) == KEY_NUM3))
-                  || ((keyCode == KEY_NUM9) && (sequencia.charAt(curSample) == KEY_NUM4)))
-                {
-                    curSample++;
-                    if (curSample == sequencia.length()) {
-                        pausa(1000);
-                        jogar();
-                    }
-                } else {
-                    for (int i = 0; i < 3; i++) {
-                        for (int x = 1; x < 6; x++) {
-                            piscaBloco(x, 100);
+                if (sequencia.length() > 0) {
+                    if (((keyCode == KEY_NUM1) && (sequencia.charAt(curSample) == KEY_NUM1))
+                     || ((keyCode == KEY_NUM3) && (sequencia.charAt(curSample) == KEY_NUM2))
+                     || ((keyCode == KEY_NUM5) && (sequencia.charAt(curSample) == KEY_NUM5))
+                     || ((keyCode == KEY_NUM7) && (sequencia.charAt(curSample) == KEY_NUM3))
+                     || ((keyCode == KEY_NUM9) && (sequencia.charAt(curSample) == KEY_NUM4))) {
+                        curSample++;
+                        if (curSample == sequencia.length()) {
+                            pausa(1000);
+                            jogar();
                         }
+                    } else {
+                        for (int i = 0; i < 3; i++) {
+                            for (int x = 1; x < 6; x++) {
+                                piscaBloco(x, 100);
+                            }
+                        }
+                        pausa(2000);
+                        novoJogo();
                     }
-                    pausa(2000);
-                    novoJogo();
                 }
             }
         }
@@ -156,7 +164,7 @@ public class NanoGenius extends Canvas implements CommandListener {
 
         sequencia.append(random.nextInt(5) + 1);
         for (int i = 0; i < sequencia.length(); i++) {
-            piscaBloco(sequencia.charAt(i) - 48, 1000);
+            piscaBloco(sequencia.charAt(i) - 48, 600);
         }
         emJogo = 0;
         curSample = 0;
