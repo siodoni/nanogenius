11fd016b7d0e05cffd490423b961679148e9e47c
siodoni@yahoo.com.br <siodoni@yahoo.com.br@a5f7f788-bef2-11de-8784-f7ad4773dbab>
2009-11-09 10:12:31 +0000

    Adicionado teste de teclas validas

--- trunk/src/nanogenius/NanoGenius.java
+++ trunk/src/nanogenius/NanoGenius.java
@@ -3,45 +3,31 @@ package nanogenius;
 import javax.microedition.lcdui.*;
 import java.util.Random;
 
-/**
- * @author Flavio A. S. Ximenes
- * @author Francisco A. Tristão
- * @author Igor Mori Tristão
- */
 public class NanoGenius extends Canvas implements CommandListener {
 
     private Main game;
-    private Command cmdSair, cmdJogar, cmdSobre, cmdHelp, cmdVoltar;
+    private Command cmdSair, cmdLoop;
     private int altura, largura, larguraAlt, percBorda, curBlock, curSample = 0, emJogo = 0, percDesloc;
     private StringBuffer sequencia = new StringBuffer();
     private static Random random = new Random();
-    private TextBox sobre, help;
 
     public NanoGenius(Main midlet) {
-        this.game = midlet;
-
-        cmdSair = new Command("Sair", Command.EXIT, 0);
-        cmdJogar = new Command("Novo", Command.ITEM, 0);
-        cmdSobre = new Command("Sobre", Command.ITEM, 0);
-        cmdHelp = new Command("Help", Command.ITEM, 0);
-        cmdVoltar = new Command("Voltar", Command.BACK, 0);
-
         altura = getHeight();
         largura = getWidth();
         percDesloc = (largura * 20) / 100;
         larguraAlt = largura - percDesloc;
         percBorda = (largura * 10) / 100;
         curBlock = 0;
-        Musica.oitava = 36;
+        Escala.oitava = 36;
+        //Escala.volume = 0;
 
+        //setFullScreenMode(true);
+        this.game = midlet;
+        cmdSair = new Command("Sair", Command.EXIT, 0);
         addCommand(cmdSair);
-        addCommand(cmdJogar);
-        addCommand(cmdSobre);
-        addCommand(cmdHelp);
+        cmdLoop = new Command("Novo", Command.ITEM, 0);
+        addCommand(cmdLoop);
         setCommandListener(this);
-
-        montaSobre();
-        montaHelp();
     }
 
     public void paint(Graphics g) {
@@ -63,19 +49,19 @@ public class NanoGenius extends Canvas implements CommandListener {
         }
 
         if (curBlock == 1) {
-            Musica.tocaNota(Musica.cNat);
+            Escala.tocaNota(Escala.cNat);
         }
         if (curBlock == 2) {
-            Musica.tocaNota(Musica.eNat);
+            Escala.tocaNota(Escala.eNat);
         }
         if (curBlock == 3) {
-            Musica.tocaNota(Musica.fNat);
+            Escala.tocaNota(Escala.fNat);
         }
         if (curBlock == 4) {
-            Musica.tocaNota(Musica.gNat);
+            Escala.tocaNota(Escala.gNat);
         }
         if (curBlock == 5) {
-            Musica.tocaNota(Musica.aNat);
+            Escala.tocaNota(Escala.aNat);
         }
 
         g.setColor(curBlock == 1 ? Cor.VERDE : Cor.VERDE_ESCURO);
@@ -97,14 +83,14 @@ public class NanoGenius extends Canvas implements CommandListener {
         g.fillArc((larguraAlt / 3) + percDesloc / 2, (larguraAlt / 3) + percDesloc, larguraAlt / 3, larguraAlt / 3, 0, 360);
     }
 
-    private void pausa(int tempo) {
+    protected void pausa(int tempo) {
         try {
             Thread.currentThread().sleep(tempo);
         } catch (InterruptedException ex) {
         }
     }
 
-    private void piscaBloco(int bloco, int tempo) {
+    protected void piscaBloco(int bloco, int tempo) {
         curBlock = bloco;
         repaint();
         serviceRepaints();
@@ -114,11 +100,11 @@ public class NanoGenius extends Canvas implements CommandListener {
         serviceRepaints();
     }
 
-    public void keyPressed(int keyCode) {
+    protected void keyPressed(int keyCode) {
 
-        //int tecla = getGameAction(keyCode);
+        int tecla = getGameAction(keyCode);
 
-        if (emJogo == 0) {
+        if ( (emJogo == 0) && (keyCode >= KEY_NUM0 && keyCode <= KEY_NUM9)) {
             if (keyCode == KEY_NUM1) {
                 piscaBloco(1, 600);
             }
@@ -135,7 +121,7 @@ public class NanoGenius extends Canvas implements CommandListener {
                 piscaBloco(4, 600);
             }
 
-            if (sequencia.length() > 0) {
+            if ( sequencia.length() > 0 )  {
                 if (((keyCode == KEY_NUM1) && (sequencia.charAt(curSample) == KEY_NUM1)) //
                         || ((keyCode == KEY_NUM3) && (sequencia.charAt(curSample) == KEY_NUM2)) //
                         || ((keyCode == KEY_NUM5) && (sequencia.charAt(curSample) == KEY_NUM5)) //
@@ -143,19 +129,19 @@ public class NanoGenius extends Canvas implements CommandListener {
                         || ((keyCode == KEY_NUM9) && (sequencia.charAt(curSample) == KEY_NUM4))) {
                     curSample++;
                     if (curSample == sequencia.length()) {
-                        emJogo = 1;
+                        emJogo=1;
                         pausa(1000);
                         jogar();
                     }
                 } else {
-                    emJogo = 1;
+                    emJogo=1;
                     for (int i = 0; i < 2; i++) {
                         for (int x = 1; x < 6; x++) {
                             piscaBloco(x, 100);
                         }
                     }
-                    if (sequencia.length() > 2) {
-                        Musica es = new Musica();
+                    if ( sequencia.length() > 2 ) {
+                        Escala es = new Escala();
                         es.tocaMusica("/fim.mp3");
                     }
                     pausa(2000);
@@ -169,14 +155,9 @@ public class NanoGenius extends Canvas implements CommandListener {
         if (c == cmdSair) {
             emJogo = 0;
             game.destroyApp(false);
-        } else if (c == cmdJogar) {
+        }
+        if (c == cmdLoop) {
             novoJogo();
-        } else if (c == cmdSobre) {
-            Display.getDisplay(game).setCurrent(sobre);
-        } else if (c == cmdHelp) {
-            Display.getDisplay(game).setCurrent(help);
-        } else if (c == cmdVoltar) {
-            Display.getDisplay(game).setCurrent(this);
         }
     }
 
@@ -193,40 +174,5 @@ public class NanoGenius extends Canvas implements CommandListener {
         }
         emJogo = 0;
         curSample = 0;
-    }
-
-    private void montaSobre() {
-        Runtime runtime = Runtime.getRuntime();
-        sobre = new TextBox(
-                "Sobre",
-                game.getAppProperty("MIDlet-Name") + "\n\n" +
-                "Versão: " + game.getAppProperty("MIDlet-Version") + "\n\n" +
-                "Autores:\n" + game.getAppProperty("MIDlet-Vendor").replace('-', '\n') + "\n\n" +
-                "Genius era um brinquedo muito popular na década de 80 " +
-                "e que buscava estimular a memorização de cores e sons. " +
-                "Com um formato semelhante a um OVNI, possuí-a botões coloridos " +
-                "que emitiam sons harmônicos e se iluminavam em sequência. " +
-                "Cabia aos jogadores repetir o processo sem errar (Wikipedia).\n\n" +
-                "Memória total: " + runtime.totalMemory() / 1024 + " kb" + "\n" +
-                "Memória livre: " + runtime.freeMemory() / 1024 + " kb",
-                500,
-                TextField.ANY | TextField.UNEDITABLE);
-
-        sobre.addCommand(cmdVoltar);
-        sobre.setCommandListener(this);
-    }
-
-    private void montaHelp() {
-        help = new TextBox(
-                "Help",
-                "Para jogar utilize as teclas 1 (verde), " +
-                "3 (vermelho), 5 (roxo), 7 (amarelo) e 9 (azul).\n\n" +
-                "A cada sequência correta o jogo irá acrescentar mais uma " +
-                "tecla na sequência.\n\nCaso o jogador erre o jogo será reiniciado " +
-                "automaticamente.",
-                500,
-                TextField.ANY | TextField.UNEDITABLE);
-        help.addCommand(cmdVoltar);
-        help.setCommandListener(this);
     }
 }
